1a0c7a32c1d4382d06a15b5aaa7702e994100c8e
Nic Laughter <nicholaslaughter@gmail.com>
2018-12-03 10:31:49 -0700

    Initial Commit

---
+++ Advent of Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Advent of Code
+//
+//  Created by Nic Laughter on 12/3/18.
+//  Copyright © 2018 Gaire Tech, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
