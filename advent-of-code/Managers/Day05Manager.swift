97e251916c508bb781ee025b006ea17fc1331e06
Nic Laughter <nicholaslaughter@gmail.com>
2018-12-04 18:35:36 -0700

    Set up for day 05

---
+++ advent-of-code/Managers/Day05Manager.swift
@@ -0,0 +1,16 @@
+//
+//  Day05Manager.swift
+//  advent-of-code
+//
+//  Created by Nic Laughter on 12/4/18.
+//  Copyright © 2018 Gaire Tech, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Day05Manager {
+    
+    let input = """
+
+"""
+}
---
+++ advent-of-codeTests/Day05Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Day05Tests.swift
+//  advent-of-codeTests
+//
+//  Created by Nic Laughter on 12/4/18.
+//  Copyright © 2018 Gaire Tech, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import advent_of_code
+
+class Day05Tests: XCTestCase {
+    
+    func test05() {
+        let manager = Day05Manager()
+    }
+}
