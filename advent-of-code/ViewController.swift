63cb92d4ab90f1c80d45f40596fd641af76bf7d2
Nic Laughter <nicholaslaughter@gmail.com>
2018-12-05 17:18:44 -0700

    Changed project to macOS app

--- advent-of-code/ViewController.swift
+++ advent-of-code/ViewController.swift
@@ -1,14 +1,14 @@
 //
 //  ViewController.swift
-//  Advent of Code
+//  advent-of-code
 //
-//  Created by Nic Laughter on 12/3/18.
+//  Created by Nic Laughter on 12/5/18.
 //  Copyright © 2018 Gaire Tech, LLC. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 
-class ViewController: UIViewController {
+class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,29 +17,30 @@ class ViewController: UIViewController {
     }
     
     func runAdvent() {
-//        print("\n\n\nDay 1")
-//        let day1 = Day01Manager()
-//        print(day1.getPosition())
-//        print(day1.checkFirstDuplicateFrequency())
-
-//        print("\n\n\nDay 2")
-//        let day2 = Day02Manager()
-//        print(day2.getChecksum())
-//        print(day2.checkMatchingID())
-
-//        print("\n\n\nDay 3")
-//        let day3 = Day03Manager()
-//        print(day3.compareCuts())
-//        print(day3.intactID())
+//                print("\n\n\nDay 1")
+//                let day1 = Day01Manager()
+//                print(day1.getPosition())
+//                print(day1.checkFirstDuplicateFrequency())
         
-//        print("\n\n\nDay 4")
-//        let day4 = Day04Manager()
-//        print(day4.computeGuardMostSlept())
-//        print(day4.computeMinuteMostSlept())
+//                print("\n\n\nDay 2")
+//                let day2 = Day02Manager()
+//                print(day2.getChecksum())
+//                print(day2.checkMatchingID())
         
-        print("Day 5")
-        let day5 = Day05Manager()
-//        print(day5.getInactivatedPolymersCount())
-        print(day5.removeProblematicUnitType())
+//                print("\n\n\nDay 3")
+//                let day3 = Day03Manager()
+//                print(day3.compareCuts())
+//                print(day3.intactID())
+        
+//                print("\n\n\nDay 4")
+//                let day4 = Day04Manager()
+//                print(day4.computeGuardMostSlept())
+//                print(day4.computeMinuteMostSlept())
+        
+                print("Day 5")
+                let day5 = Day05Manager()
+//                print(day5.getInactivatedPolymersCount())
+                print(day5.removeProblematicUnitType())
     }
 }
+
